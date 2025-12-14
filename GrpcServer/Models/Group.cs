cac852b703a38e7e8092a91d2930387ea752f0c7
Thilak Kanala <51452386+thilak-kanala@users.noreply.github.com>
2025-12-14 15:55:38 -0500

    back 2 scratch

---
+++ GrpcServer/Models/Group.cs
@@ -0,0 +1,7 @@
+namespace GrpcServer.Models;
+
+public class Group
+{
+    public int Id { get; set; }
+    public string DisplayName { get; set; } = string.Empty;
+}
--- GrpcServer/Models/User.cs
+++ GrpcServer/Models/User.cs
@@ -1,28 +1,8 @@
-namespace GrpcServer.Models
+namespace GrpcServer.Models;
+
+public class User
 {
-    public class User(string id, string userName, bool isActive)
-    {
-        public string Id { get; } = id;
-        public string UserName { get; } = userName;
-        public bool IsActive { get; } = isActive;
-        
-
-        public override string ToString()
-        {
-            return $"User [Id={Id}, UserName={UserName}, IsActive={IsActive}]";
-        }
-
-        public override bool Equals(object? obj)
-        {
-            if (ReferenceEquals(this, obj)) return true;
-            if (obj is null || GetType() != obj.GetType()) return false;
-            var other = (User)obj;
-            return Id == other.Id && UserName == other.UserName;
-        }
-
-        public override int GetHashCode()
-        {
-            return HashCode.Combine(Id, UserName);
-        }
-    }
+    public string Email { get; set; } = string.Empty;
+    public string UserName { get; set; } = string.Empty;
+    public int Id { get; set; }
 }
