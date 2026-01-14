018d2808df3be12f6d512ed1e1e8b882ed6665f7
Thilak Kanala <51452386+thilak-kanala@users.noreply.github.com>
2026-01-14 11:26:31 -0500

    refactor: simplify TstMapper class formatting for improved readability

--- GrpcServer/Infrastructure/Mappers/TST/TstMapper.cs
+++ GrpcServer/Infrastructure/Mappers/TST/TstMapper.cs
@@ -2,71 +2,70 @@ using GrpcServer.Infrastructure.DTO.TST;
 using GrpcServer.Infrastructure.Mappers.Common;
 using GrpcServer.Infrastructure.Models.TST;
 
-namespace GrpcServer.Infrastructure.Mappers.TST
+namespace GrpcServer.Infrastructure.Mappers.TST;
+
+public sealed class TstMapper : 
+    IMapper<TstUser, TstUserRequestDto, TstUserResponseDto>,
+    IMapper<TstGroup, TstGroupRequestDto, TstGroupResponseDto>
 {
-    public sealed class TstMapper : 
-        IMapper<TstUser, TstUserRequestDto, TstUserResponseDto>,
-        IMapper<TstGroup, TstGroupRequestDto, TstGroupResponseDto>
+    public TstUserResponseDto ToResponseDto(TstUser entity)
     {
-        public TstUserResponseDto ToResponseDto(TstUser entity)
-        {
-            return new TstUserResponseDto(
-                entity.Id,
-                entity.UserName,
-                entity.Email,
-                entity.TstUserExtension1,
-                entity.TstUserExtension2
-            );
-        }
+        return new TstUserResponseDto(
+            entity.Id,
+            entity.UserName,
+            entity.Email,
+            entity.TstUserExtension1,
+            entity.TstUserExtension2
+        );
+    }
 
-        public TstUser FromRequestDto(TstUserRequestDto dto)
+    public TstUser FromRequestDto(TstUserRequestDto dto)
+    {
+        return new TstUser
         {
-            return new TstUser
-            {
-                Id = dto.Id,
-                UserName = dto.UserName,
-                Email = dto.Email,
-                TstUserExtension1 = dto.TstUserExtension1,
-                TstUserExtension2 = dto.TstUserExtension2
-            };
-        }
+            Id = dto.Id,
+            UserName = dto.UserName,
+            Email = dto.Email,
+            TstUserExtension1 = dto.TstUserExtension1,
+            TstUserExtension2 = dto.TstUserExtension2
+        };
+    }
 
-        public void ApplyPatch(TstUser entity, TstUserRequestDto dto)
-        {
-            entity.Id = dto.Id;
-            entity.UserName = dto.UserName;
-            entity.Email = dto.Email;
-            entity.TstUserExtension1 = dto.TstUserExtension1;
-            entity.TstUserExtension2 = dto.TstUserExtension2;
-        }
+    public void ApplyPatch(TstUser entity, TstUserRequestDto dto)
+    {
+        entity.Id = dto.Id;
+        entity.UserName = dto.UserName;
+        entity.Email = dto.Email;
+        entity.TstUserExtension1 = dto.TstUserExtension1;
+        entity.TstUserExtension2 = dto.TstUserExtension2;
+    }
 
-        public TstGroupResponseDto ToResponseDto(TstGroup entity)
-        {
-            return new TstGroupResponseDto(
-                entity.Id,
-                entity.DisplayName,
-                entity.TstGroupExtension1,
-                entity.TstGroupExtension2
-            );
-        }
+    public TstGroupResponseDto ToResponseDto(TstGroup entity)
+    {
+        return new TstGroupResponseDto(
+            entity.Id,
+            entity.DisplayName,
+            entity.TstGroupExtension1,
+            entity.TstGroupExtension2
+        );
+    }
 
-        public TstGroup FromRequestDto(TstGroupRequestDto dto)
+    public TstGroup FromRequestDto(TstGroupRequestDto dto)
+    {
+        return new TstGroup
         {
-            return new TstGroup
-            {
-                Id = dto.Id,
-                DisplayName = dto.DisplayName,
-                TstGroupExtension1 = dto.TstGroupExtension1,
-                TstGroupExtension2 = dto.TstGroupExtension2
-            };
-        }
+            Id = dto.Id,
+            DisplayName = dto.DisplayName,
+            TstGroupExtension1 = dto.TstGroupExtension1,
+            TstGroupExtension2 = dto.TstGroupExtension2
+        };
+    }
 
-        public void ApplyPatch(TstGroup entity, TstGroupRequestDto dto)
-        {
-            entity.Id = dto.Id;
-            entity.DisplayName = dto.DisplayName;
-            entity.TstGroupExtension1 = dto.TstGroupExtension1;
-            entity.TstGroupExtension2 = dto.TstGroupExtension2;
-        }
+    public void ApplyPatch(TstGroup entity, TstGroupRequestDto dto)
+    {
+        entity.Id = dto.Id;
+        entity.DisplayName = dto.DisplayName;
+        entity.TstGroupExtension1 = dto.TstGroupExtension1;
+        entity.TstGroupExtension2 = dto.TstGroupExtension2;
     }
 }
